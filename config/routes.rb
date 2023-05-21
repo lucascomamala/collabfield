2e6d2efa6736b0f2184de55200c07cb28cc7308c
lucascomamala <lucascomamala@gmail.com>
2023-05-21 01:06:15 -0600

    buncha stuff

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   
   devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
+    get 'login', to: 'devise/sessions#new'
   end
 
   root to: 'pages#index'
